c09c6e9e1190a5e37af2a486b3662d0a1a5e8a4b
Jeremias Quinteros <jeremias8quinteros@gmail.com>
2022-12-07 14:41:21 -0300

    websockets

---
+++ Clase-11/server.js
@@ -0,0 +1,52 @@
+const express= require("express")
+const path= require("path")
+const SocketIO= require("socket.io")
+const handlebars= require("express-handlebars")
+
+const app= express()
+app.use(express.static(path.join(__dirname, "public")))
+const productos= [
+    {
+        name:"Sillon",
+        price:400,
+        thumbnail:"https://silloneseuropa.com.ar/wp-content/uploads/2020/06/sam-1.jpg"
+    }
+]
+
+const mensajes=[]
+
+const fecha= new Date()
+
+// middlewares
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.engine("handlebars", handlebars.engine())
+app.set("views", "./views")
+app.set("view engine", "handlebars")
+
+
+
+app.get("/FormularioYProductos", (req, res)=>{
+    res.render("productos", {productos, mensajes, fecha})
+})
+
+const server= app.listen(3000, ()=> console.log("Server on http://localhost:3000"))
+
+const io= SocketIO(server)
+
+io.on("connection",(socket)=>{
+    console.log("New connection")
+    socket.on("create:data",(data)=>{
+        productos.push(data)
+        io.sockets.emit("create:data", productos)
+    })
+
+    socket.on("chat:message", (data)=>{
+        let data2= {...data, date:fecha.getHours()+ ":" + fecha.getMinutes()}
+        mensajes.push(data2)
+        io.sockets.emit("chat:message", data)
+        console.log(mensajes)
+    })
+    
+})
